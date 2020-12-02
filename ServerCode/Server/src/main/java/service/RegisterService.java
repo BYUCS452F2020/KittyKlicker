9be4f6a5ec19669fcad4773b8beb75a6cd8c27af
unknown <jaredaswensen@gmail.com>
2020-12-01 20:23:48 -0700

    Add updated Login/Register services

--- ServerCode/Server/src/main/java/service/RegisterService.java
+++ ServerCode/Server/src/main/java/service/RegisterService.java
@@ -1,23 +1,43 @@
 package service;
 
 import dao.DataBase;
+import dao.PowerUpDao;
+import dao.TeamDao;
 import dao.UserDao;
 import model.AuthToken;
+import model.PowerUp;
+import model.Team;
 import model.User;
 import request.RegisterRequest;
 import response.Login_RegisterResponse;
 import response.Response;
 
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Random;
 
 /**
 * Register Service
 */
 public class RegisterService {
 
-   private static DataBase db = DataBase.getInstance();
+    private static DataBase db = DataBase.getInstance();
 
-   /**
+    public static final String  QUOTES[] = {
+            "Be yourself; everyone else is already taken.― Oscar Wilde",
+            "A room without books is like a body without a soul. ― Marcus Tullius Cicero",
+            "Be the change that you wish to see in the world. ― Mahatma Gandhi",
+            "If you tell the truth, you don't have to remember anything. ― Mark Twain",
+            "If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.― J.K. Rowling",
+            "To live is the rarest thing in the world. Most people exist, that is all.― Oscar Wilde",
+            "Without music, life would be a mistake. ― Friedrich Nietzsche",
+            "Always forgive your enemies, nothing annoys them so much. ― Oscar Wilde",
+            "Life isn't about getting and having, it's about giving and being. –Kevin Kruse",
+            "Whatever the mind of man can conceive and believe, it can achieve. –Napoleon Hill",
+            "Strive not to be a success, but rather to be of value. –Albert Einstein",
+    };
+
+    /**
     * RegisterService Constructor
     */
    public RegisterService createService() {
@@ -45,13 +65,25 @@ public class RegisterService {
                return new Response("Username is already taken.");
            }
 
+           Team team = TeamDao.find(request.getTeamName());
+
+           if (team == null)
+           {
+               Random rand = new Random();
+
+               team = new Team(request.getTeamName(), QUOTES[rand.nextInt(QUOTES.length)], 0);
+               TeamDao.insert(team);
+           }
+
            User user = new User(request.getUserName(), request.getPassword(), 0, 1, request.getTeamName());
 
            UserDao.insert(user);
 
+           List<PowerUp> powerups = PowerUpDao.find(user.getUserID());
+
            AuthToken auth = AuthService.generate(user.getUserID());
 
-           return new Login_RegisterResponse(auth.getToken(), user.getUserID(), user.getTeamName(), user.getKittiesKlicked(), user.getKittiesPerKlick(), null);
+           return new Login_RegisterResponse(auth.getToken(), user, team, powerups);
        }
        catch (Exception e)
        {
